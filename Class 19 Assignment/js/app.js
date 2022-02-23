0320c28b789d0e6c3418b38916cc138fac4bf836
Rohan-99ctrl <rohonmostofa9900@gmail.com>
2022-02-23 17:04:36 +0600

    updated

--- Class 19 Assignment/js/app.js
+++ Class 19 Assignment/js/app.js
@@ -56,16 +56,16 @@ const getDevelopers = () => {
                     <td>${devsDataItem.id}</td>
                     <td>${devsDataItem.nam}</td>
                     <td>${devsDataItem.numb}</td>
-                    <td>${devsDataItem.salary}</td>
+                    <td>$${devsDataItem.salary}</td>
                     <td><img src="${devsDataItem.photo}" alt=""></td>
                     <td> 
-                        <a data-bs-toggle="modal" href="#myModalView">
+                        <a id="view" onclick="viewDeveloperDetails(${devsDataItem.id})" data-bs-toggle="modal" href="#myModalView">
                             <button class="btn btn-info btn-sm"><i class="fas fa-eye"></i></button>
                         </a> 
-                        <a data-bs-toggle="modal" href="#myModalEdit">
+                        <a id="edit" onclick="viewDeveloperedit(${devsDataItem.id})" data-bs-toggle="modal" href="#myModalEdit">
                             <button class="btn btn-warning btn-sm"><i class="fas fa-user-edit"></i></button>
                         </a> 
-                        <a data-bs-toggle="modal" href="#myModalDelete">
+                        <a id="delete" onclick="viewDeveloperDelete(${devsDataItem.id})" data-bs-toggle="modal" href="#myModalDelete">
                             <button class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>
                         </a> 
                     </td>
@@ -84,8 +84,46 @@ getDevelopers();
 
 
 
-const devsAddForm = document.querySelector('#devsAddForm');
+// view data
 
+const uploadDevsDataDetails = document.querySelector('#uploadDevsDataDetails');
+
+function viewDeveloperDetails(id){
+
+    axios.get(`http://localhost:2020/devs/${id}`).then((response) => {
+
+        uploadDevsDataDetails.innerHTML = `
+            <div class="detailWrapper mb-4 mt-2">
+                <div class="row">
+                    <div class="col-md-5 imageWrapper">
+                        <img style="padding: 8px; background-color: rgb(41, 41, 41);width: 250px;height: 300px;object-fit: cover;" src="${response.data.photo}" alt="">
+                    </div>
+
+                    <div class="col-md-7 detailContent">
+                        <h4><span style="color: blueviolet;">Name :</span> ${response.data.nam}</h4>
+                        <h4><span style="color: blueviolet;">Age :</span> ${response.data.age}</h4>
+                        <h4><span style="color: blueviolet;">Location :</span> ${response.data.location}</h4>
+                        <h4><span style="color: blueviolet;">Contact No. :</span> ${response.data.numb}</h4>
+                        <h4><span style="color: blueviolet;">E-mail :</span> ${response.data.mail}</h4>
+                        <h4><span style="color: blueviolet;">Skill :</span> ${response.data.skillId}</h4>
+                        <h4><span style="color: blueviolet;">Salary :</span> $${response.data.salary}</h4>
+                    </div>
+                </div>
+            </div>
+        `;
+
+    })
+}
+
+
+// edit data 
+
+
+
+
+
+
+const devsAddForm = document.querySelector('#devsAddForm');
 
 devsAddForm.addEventListener('submit', function(event){
 
@@ -131,11 +169,11 @@ devsAddForm.addEventListener('submit', function(event){
 
         });
 
-        
-
     }
 
 })
+
+
 
 
 
